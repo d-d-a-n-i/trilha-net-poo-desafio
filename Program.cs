66ee8c57cea1dcf73a1b2c3e754254cd58f76c2d
dani <daniel.ssouza.f@gmail.com>
2024-05-25 13:19:13 -0300

    d-d-o-n-e

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new("1232141", "m1", "002", 8);
+nokia.InstalarAplicativo("WHATAPP GB");
+
+Iphone iphone = new("1412312", "1m", "001", 2);
+iphone.InstalarAplicativo("SHIN MEGAMI TENSEI I");
